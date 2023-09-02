388998ee095a0ab48b9cd3772abff0b0add4b52a
David <ciucadavidstefan@gmail.com>
2023-09-02 21:31:18 +0300

    same functionality with different data structure

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+
+
+let bookmarks = [];
+
 // Show Modal, Focus on Input
 
 function showModal() {
     modal.classList.add('show-modal');
     websiteNameEl.focus();
-    
+
 };
 
 
@@ -24,6 +28,8 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+
 
 // Validate Form
 function validate(nameValue, urlValue) {
@@ -41,6 +47,82 @@ function validate(nameValue, urlValue) {
     return true;
 };
 
+// Build Bookmarks DOM
+function buildBookmarks() {
+    // Remove all bookmark elements :::
+    bookmarksContainer.textContent = '';
+    // Build items ---- forEach(bookmark) - creates a local variable to represent each object in the array
+    bookmarks.forEach((bookmark) => {
+
+        // destructuring:
+        const { name, url } = bookmark;
+
+        // Creating HTML elements: starting with Item
+        const item = document.createElement('div');
+        item.classList.add('item');
+
+        // Close Icon
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fas', 'fa-times');
+        closeIcon.setAttribute('title', 'Delete Bookmark');
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+
+        // Favicon / Link Container
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+
+        // Favicon
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute('alt', 'Favicon');
+
+        // Link
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`);
+        link.setAttribute('target', '_blank');
+        link.textContent = name;
+
+        // Appent to bookmarks container - building has to happen in a specific order
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo);
+        bookmarksContainer.appendChild(item);
+    });
+};
+
+
+// Fetch Bookmarks from localStorage
+function fetchBookmarks() {
+    // Get bookmarks from localStorage if available
+    if (localStorage.getItem('bookmarks')) {
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+        // Create bookmarks array in localStorage
+        bookmarks = [
+            {
+                name: 'Default Google Bookmark',
+                url: 'https://google.com',
+            },
+        ];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    };
+    buildBookmarks();
+};
+
+
+// Delete Bookmark
+function deleteBookmark(url) {
+    bookmarks.forEach((bookmark, i) => {
+        if (bookmark.url === url) {
+            // i - index; 1 - delete one item
+            bookmarks.splice(i, 1);
+        };
+    });
+    // Update bookmarks arrya in localStorage, re-populate the DOM
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+};
+
+
 // Handle data form Form
 function storeBookmark(e) {
     e.preventDefault();
@@ -50,10 +132,19 @@ function storeBookmark(e) {
     if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
         urlValue = `https://${urlValue}`;
     };
-    console.log(nameValue, urlValue);
+
     if (!validate(nameValue, urlValue)) {
         return false;
     };
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
 };
 
 // by defaul a form it's something that is usually submitted to a web server so it usually makes a network request. e.preventDefault()
@@ -61,3 +152,6 @@ function storeBookmark(e) {
 
 // Bookmark Store event Listener
 bookmarkForm.addEventListener('submit', storeBookmark);
+
+// On Load, fetch bookmarks
+fetchBookmarks();
